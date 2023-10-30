3fb7d1999680fdff9526c6be0f2e09ef65d82aca
nandini <ramavath.nandini0026@gmail.com>
2023-10-30 11:15:56 +0530

    first commit

---
+++ src/main/java/SEGroup/NandiniMavenJava/App.java
@@ -0,0 +1,13 @@
+package SEGroup.NandiniMavenJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello from Nandini.This is my first  software engineering lab project" );
+    }
+}
